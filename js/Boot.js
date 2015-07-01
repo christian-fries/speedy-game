b09cc27f44c111beee6ced8031a8f7dd3daa381f
Christian Fries <christian.fries@lstschenker.ch>
2015-07-01 10:33:34 +0200

    Disable scaling of the game

--- js/Boot.js
+++ js/Boot.js
@@ -12,14 +12,11 @@ EdubookGame.Boot.prototype = {
 
     create: function() {
         this.stage.disableVisibilityChange = false;
-        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        //this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
         this.scale.minWidth = 480;
         this.scale.minHeight = 320;
-        /*this.scale.maxWidth = 800;
-        this.scale.maxHeight = 600;*/
         this.scale.pageAlignHorizontally = true;
         this.scale.pageAlignVertically = true;
-        this.scale.setScreenSize(true);
         this.stage.backgroundColor = '#92bc00';
 
         this.state.start('Preloader');
